ea096a2b4653fa3941358f79ab449c8340ea6a37
nyone4ka <107935206+nyone4ka@users.noreply.github.com>
2025-12-14 13:12:36 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,2 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
-{
-    int a,b,c;
-    printf("Enter the value of a and b\n");
-    scanf("%d%d",&a,&b);
-    c=a+b;
-    printf("The sum of %d and %d is %d\n",a,b,c);
-    return 0;
-}
-
-// This code is written in C17 standard version. // This code is written in C17 standard version. // This code is written in C17 standard version. уццйуцйу
-// This code is written in C17 standard version. // This code is written in C17 standard version. // This code is written in C17 standard version. // This code is written in C17 standard version.
-// This code is written in C17 standard version. // This code is written in C17 standard version. // This code is written in C17 standard version.
